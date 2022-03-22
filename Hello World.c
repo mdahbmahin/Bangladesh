715d5243893b439948918bcf5bf5468d3c5c2b23
Md. Ahb Mahin <89274582+mdahbmahin@users.noreply.github.com>
2022-03-22 15:18:20 +0600

    Update Hello World.c

--- Hello World.c
+++ Hello World.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main(){
 
-printf("Hello C Program");
+printf("Hello Bangladesh");
+printf("Hello Windows1 - 7CZN7-BGNZ5-MW6VV-TVYTM-3AWS6");  
+printf("Hello Windows2 - SAHKR-2ZTRR-BVVA3-EYJVB-XKRBH ");
+printf("Hello Windows3 - KW5P4-Q44WG-9L66V-GQM84-L4MFH ");
+
 
 return 0;
 }
